6bedfe2431ec8d52d514b8c70f055b452fac77a8
martin <tlkuo.frb@gmail.com>
2016-04-13 19:52:52 +0800

    Add table sections content

--- ShaRead/ShaRead/MainViewController.swift
+++ ShaRead/ShaRead/MainViewController.swift
@@ -13,6 +13,13 @@ class MainViewController: UIViewController {
     @IBOutlet weak var mainTableView: UITableView!
     @IBOutlet weak var searchButton: UIButton!
 
+    let sectionTitleArray: [String] = [
+        "店長推薦書籍",
+        "最近瀏覽",
+        "熱門書店",
+        "最新書店"
+    ]
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +27,10 @@ class MainViewController: UIViewController {
         // Adjust table header height to half of screen
         if let headerView = mainTableView.tableHeaderView {
             headerView.frame.size.height = UIScreen.mainScreen().bounds.height / 2
+        }
+
+        if let footerView = mainTableView.tableFooterView {
+            footerView.frame.size.height = 0
         }
 
         // Expand table row height according to content
@@ -42,8 +53,16 @@ class MainViewController: UIViewController {
 
 extension MainViewController: UITableViewDataSource, UITableViewDelegate {
 
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return sectionTitleArray.count
+    }
+
+    func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return sectionTitleArray[section]
+    }
+
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return 1
     }
 
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
