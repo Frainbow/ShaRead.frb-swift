51f551c0ec1a62e1cba626e78c053efab7273861
martin <tlkuo.frb@gmail.com>
2016-04-20 22:20:08 +0800

    Enhance UX of search text filed

--- ShaRead/ShaRead/SearchViewController.swift
+++ ShaRead/ShaRead/SearchViewController.swift
@@ -11,20 +11,37 @@ import UIKit
 class SearchViewController: UIViewController {
 
     @IBOutlet weak var scanButton: UIButton!
+    @IBOutlet weak var searchTextField: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         scanButton.layer.cornerRadius = 5
     }
+    
+    override func viewWillAppear(animated: Bool) {
+        searchTextField.becomeFirstResponder()
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        searchTextField.resignFirstResponder()
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
+
     @IBAction func dismissSearch(sender: AnyObject) {
         self.dismissViewControllerAnimated(true, completion: nil)
+    }
+
+    @IBAction func dismissKeyboard(sender: AnyObject) {
+        self.navigationController?.view.endEditing(true)
+    }
+
+    @IBAction func endOnExit(sender: AnyObject) {
+        self.resignFirstResponder()
     }
 
     /*
